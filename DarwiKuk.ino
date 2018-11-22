3e77eeb8c755d2eb8dfc8bf68b704470f821e8a0
Termiter <31243547+Termiter@users.noreply.github.com>
2018-11-22 13:45:57 +0100

    1.4-5 2018-02-22

--- DarwiKuk.ino
+++ DarwiKuk.ino
@@ -1,3 +1,4 @@
+unsigned long zacatek = millis(); //uloží si aktuální čas kvůli měření trvání doby běhu skriptu, o ten pak zkrátí čekání do dalšího cyklu
 //    =======================================================
 //    =                                                     =
 //    =                     DarwiKuk                        =
@@ -8,11 +9,23 @@
 //    =                                                     =
 //    =======================================================
 //
-//    DarwiKuk 1.3-4 2018-02-05
-//      * Opravena funkce na čtení MAC adresy
-//      * Přidáno číslo verze FW do záznamu
+//    DarwiKuk 1.4_5 2018-02-22
+String ver = "5";
+//      * Optimalizace proměnných
+//      * Zkrácení čekání před odpojením WiFi na 5 sekund
+//      * LEDka svítí krátce jen během připojování WiFi, případně stále, pokud je nastaven mód pro vložení SSID a hesla
+//      * Zpřesnění měření času potřebného pro spánek
+//      * Nahrazní funkce delay(), která v některých případech dělala WDT reset
+//      * Odstranění ověřování dostupnosti serveru, použije se přímo zápis měření
+//      * Přidaná prodleva 5 sekund pro zpracování http get dotazu
  
-String ver = "4";
+ 
+//prodleva mezi měřeními v sekundach
+unsigned int prodleva = 60 * 5 + 21; //5 minut, +21 korekce
+//unsigned int prodleva = 30; //pro testování
+unsigned int poWiFi = 5 * 1000; //čas na odpojení WiFi před restartem
+String newData = "http://iot.darwiniana.cz/new_data.php"; //sem posílá data
+const unsigned int pocetMereni = 16; //počet měření kvůli přesnoti, musí být dělitelný 4
  
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <DNSServer.h>
@@ -20,27 +33,15 @@ String ver = "4";
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 #include <ESP8266HTTPClient.h>
  
-// LED_PIN 2 neboli GPI02 neboli D4, interní LED která oznamuje komunikaci a nedostupnost internetu
-#define LED_PIN 2
- 
-// 74800 je rychlost na které NodeMCU komunikuje po startu a vypisuje chybové hlášky, takže je vidět všechno co se děje
-#define SER 74880
- 
-long zacatek = millis(); //uloží si aktuální čas kvůli měření trvání doby běhu skriptu, o ten pak zkrátí čekání do dalšího cyklu
-//prodleva mezi měřeními v sekundach
-int prodleva = 60 * 5; //5 minut
-//int prodleva = 30; //pro testování
-int poWiFi = 10 * 1000; //čas na odpojení WiFi před restartem
+#define LED_PIN 2 // LED_PIN 2 neboli GPI02 neboli D4, interní LED která oznamuje komunikaci a nedostupnost internetu
+#define SER 115200 // 74800 je rychlost na které NodeMCU komunikuje po startu. Na 115200 vypisuje chybové hlášky, takže je vidět všechno co se děje
  
 String MAC(12); //proměnná kde se bude nacházet MAC adresa WiFi, používá se pro identifikaci měřidla v databázi
-String testconn = "http://iot.darwiniana.cz/testconn.txt"; //stránka obsahuje znak 1 a slouží k otestování připojení
-String newData = "http://iot.darwiniana.cz/new_data.php"; //sem posílá data
  
-const int pocetMereni = 16; //počet měření kvůli přesnoti, musí být dělitelný 4
 float mereni[pocetMereni]; //pro ukládání jednotlivých měření
  
 // WiFiManager zajišťuje přepnutí na AP a vložení SSID a hesla
-int wifiTimeout = 240; // po x sekundách zkusí znovu připojení k internetu, vhodné po výpadku WiFi, standardní hodnota 240s, 4 minuty
+unsigned int wifiTimeout = 240; // po x sekundách zkusí znovu připojení k internetu, vhodné po výpadku WiFi, standardní hodnota 240s, 4 minuty
 WiFiManager wifiManager;
  
 // nastavení sběrnice SCL --> D1, SDA --> D2, default I2C PINy u NodeMCU
@@ -87,12 +88,25 @@ float hodnotaMereni() {
 }
  
 //================================================
+// místo delay();
+void pockat(unsigned long pockat) {
+  Serial.print(pockat / 1000); Serial.println(" sekund");
+  unsigned long i = millis();
+  unsigned long j;
+  do {
+    j = millis();
+    ESP.wdtFeed(); //vynuluje WDT, jinak dojde k WDT resetu
+  }
+  while (j - i < pockat);
+}
+ 
+//================================================
 void setup() {
   Serial.begin(SER); //nastaví sériovou linku pro debug informace
   Serial.println(); Serial.println(); Serial.println();
  
   pinMode(LED_PIN, OUTPUT); //oznamovací LED
-  digitalWrite(LED_PIN, LOW); // hned po startu rozsviti LEDku
+  digitalWrite(LED_PIN, HIGH); // hned po startu zhasne LEDku, ta bude svítit jen při nastavování WiFi
  
   Wire.begin();
  
@@ -109,96 +123,88 @@ void setup() {
   wifiManager.setAPStaticIPConfig(IPAddress(10, 10, 10, 10), IPAddress(10, 10, 10, 10), IPAddress(255, 255, 255, 0)); //nastaví adresu AP
   wifiManager.setConfigPortalTimeout(wifiTimeout);
  
-  //nepokračuje, dokud není přístupný server pro ukládání údajů
-  wifiManager.autoConnect("DarwiKuk");
-  HTTPClient http;
- 
-  boolean inet = false;
-  Serial.print("Testuji pripojeni na ");
-  Serial.println(testconn);
-  http.begin(testconn);
-  int httpCode = http.GET(); //zavolá stránku pro test dostupnosti serveru
-  String payload = http.getString();
-  if (payload.charAt(0) == '1') {
-    inet = true;
-  }
-  Serial.print("Vratilo se: ");
-  Serial.println(payload);
- 
-  if (!inet) { //nepodařilo se připojit k AP, případně není dostupný server
-    Serial.println("Ackoliv je WiFi pripojena, nepodarilo se kontakovat server, restart!");
-    WiFi.mode(WIFI_OFF); //vypne WiFi
-    delay(poWiFi);//počká poWiFI sekund na vypnutí WiFi
-    //ESP.deepSleep(1e6);//za jednu sekundu se restartuje
-    ESP.restart();
-  }
- 
   //začneme měřit
+  Serial.println("Zaciname merit.");
   //načte do h vlhkost
+  Serial.print("h = ");
   for (int i = 0; i < pocetMereni; i++) {
     mereni[i] = bme.readHumidity();
   }
   float h = hodnotaMereni();
+  Serial.println(h);
  
   //načte do t teplotu
+  Serial.print("t = ");
   for (int i = 0; i < pocetMereni; i++) {
     mereni[i] = bme.readTemperature();
   }
   float t = hodnotaMereni();
+  Serial.println(t);
  
   //načte tlak
+  Serial.print("p = ");
   for (int i = 0; i < pocetMereni; i++) {
     mereni[i] = bme.readPressure();
   }
   float p = hodnotaMereni();
+  Serial.println(p);
  
   //načte do l osvětlení
+  Serial.print("l = ");
   for (int i = 0; i < pocetMereni; i++) {
     sensors_event_t event;
     tsl.getEvent(&event);
     mereni[i] = event.light;
   }
   float l = hodnotaMereni();
- 
-  //vypíše načtené údaje
-  Serial.print("h = ");
-  Serial.println(h);
-  Serial.print("t = ");
-  Serial.println(t);
-  Serial.print("p = ");
-  Serial.println(p);
-  Serial.print("l = ");
   Serial.println(l);
+  Serial.println();
+ 
+  //nepokračuje, dokud není připojený na WiFi, případně vyvolá mód pro vložení SSID a hesla
+  digitalWrite(LED_PIN, LOW); //rozsvítí LEDku, pokud svítí stále, uživatel ví, že má zadat heslo
+  wifiManager.autoConnect("DarwiKuk");
+  digitalWrite(LED_PIN, HIGH); //zhasne LEDku
+  HTTPClient http;
  
   //pošle data jako GET
   String data = newData + "?mac=" + MAC + "&t=" + String(t) + "&h=" + String(h) + "&p=" + String(p) + "&l=" + String(l) + "&ver=" + ver;
   Serial.println(data);
   http.begin(data);
-  httpCode = http.GET();
-  payload = http.getString();   //odešle data na server, zpět by se měla vrátit 1
-  Serial.print("Server vratil: ");
-  Serial.println(payload);
-  if (payload == "1") {
-    Serial.println("Data uspesne zapsana na server.");
+  Serial.print("Dame serveru cas na vyrizeni pozadavku ");
+  pockat (5 * 1000); //dáme serveru chvilku na odpověď
+  boolean zapsano = false;
+  int httpCode = http.GET();//odešle data na server, zpět by se měla vrátit 1
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.print("Server vratil: ");
+    Serial.println(payload);//načte odeslaná data, měla by tm být 1
+    if (payload == "1") {
+      Serial.println("Data uspesne zapsana na server.");
+      zapsano = true;
+    }
+  }
+  else {
+    Serial.println("Chyba http");
   }
  
-  else {
-    Serial.println("Nepovedlo se připojit se na server. Restart!");
+  if (!zapsano) {
     WiFi.mode(WIFI_OFF); //vypne WiFi
-    delay(poWiFi);//počká poWiFI sekund na vypnutí WiFi
+    Serial.print("Nepovedlo se připojit se na server. Restart za ");
+    pockat(poWiFi);//počká poWiFI sekund na vypnutí WiFi
     ESP.deepSleep(1e6);//za jednu sekundu se restartuje
   }
  
   http.end();   //uzavře HTTP spojení
   WiFi.mode(WIFI_OFF); //vypne WiFi
-  delay(poWiFi);//počká poWiFI sekund na vypnutí WiFi
-  digitalWrite(LED_PIN, HIGH); // zhasne LEDku
+  Serial.print("Pro ukonceni WiFi cekam ");
+  pockat(poWiFi);//náhradní funkce za delay()
  
-  int spat = prodleva - ((millis() - zacatek ) / 1000);
-  Serial.print("Jdu spat na ");
-  Serial.print(spat);
-  Serial.println(" sekund");
-  ESP.deepSleep(spat * 1e6);
+  long spat = (prodleva * 1000) - ((millis() - zacatek ));
+  if (spat < 1) {
+    spat = 1;
+  }
+  Serial.print("Jdu spat na "); Serial.print(spat / 1000); Serial.println(" sekund");
+  ESP.deepSleep(spat * 1000); //tady je to mikrosekundách
 }
  
 //================================================
